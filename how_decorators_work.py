efb43f0b694379f4708cfbf894d60ea8026a46f3
Sizov <wwwwwwwww@list.ru>
2022-10-17 02:31:23 +0300

    добавил пояснения

--- how_decorators_work.py
+++ how_decorators_work.py
@@ -5,8 +5,13 @@ def my_new_dec(function):
         print('работает после оригинальной функции')
     return wrapper  # возвращает обертку вокруг оригинала + оригинал
 
+@my_new_dec
 def original_function():
     print('это попадет в декоратор')
 
-original_function_decorated = my_new_dec(original_function)
-original_function_decorated()
+original_function()
+# original_function_decorated = my_new_dec(original_function)
+# original_function_decorated()
+
+# запись выше эквивалентна следующей:
+
