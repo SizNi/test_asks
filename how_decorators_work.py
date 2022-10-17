0a3023540a6e72d6b00fa40d59f507f9251afad9
Sizov <wwwwwwwww@list.ru>
2022-10-17 03:03:12 +0300

    добавил пояснения

--- how_decorators_work.py
+++ how_decorators_work.py
@@ -1,17 +1,18 @@
 def my_new_dec(function):
-    def wrapper():
+    def wrapper(arg1, arg2):  # аргументы передаются сюда и в оригинальную функцию ниже
         print('работает перед оригинальной функцией')
-        function()  # вызов оригинальной функции
+        function(arg1, arg2)  # вызов оригинальной функции
         print('работает после оригинальной функции')
     return wrapper  # возвращает обертку вокруг оригинала + оригинал
 
 @my_new_dec
-def original_function():
-    print('это попадет в декоратор')
+def original_function(first_name, last_name):
+    print(f'это попадет в декоратор {first_name} {last_name}')
 
-original_function()
+original_function('Nik', 'Siz')
+# запись выше эквивалентна следующей:
 # original_function_decorated = my_new_dec(original_function)
 # original_function_decorated()
 
-# запись выше эквивалентна следующей:
+
 
