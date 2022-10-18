ba4379edad3bb0c2bd60109c6fe694354a442969
Sizov <wwwwwwwww@list.ru>
2022-10-18 03:14:50 +0300

    typo

--- del_part_list.py
+++ del_part_list.py
@@ -1,16 +1,16 @@
-def keep_odds_from_odds(list_list):
-    i = 1
+def keep_odds_from_odds(list_list):  # процедурное решение для удаления элементов
+    i = 1  # два счетчика нужно для компенсации уменьшения длины списка после удаления элемента
     prev_i = 0
     while prev_i < len(list_list):
         if list_list != [] and list_list != [[]]:
-            if i == prev_i:
+            if i == prev_i:  # тут удаляется элемент внешнего списка если он нечетный
                 del list_list[i]
                 i += 1
-            else:
-                i_i = 1
+            else:  # тут происходит работа с четными вложенными списками
+                i_i = 1  # два счетчика нужно для компенсации уменьшения длины списка после удаления элемента
                 prev_i_i = 0
                 while i_i < len(list_list[prev_i]):
-                    if i_i == prev_i_i:
+                    if i_i == prev_i_i:  # удаление четного элемента списка
                         del list_list[prev_i][i_i]
                         i_i += 1
                     else:
