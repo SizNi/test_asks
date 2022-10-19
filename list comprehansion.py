616fc73863fb08ad872567f21f9061b9195098c6
Sizov <wwwwwwwww@list.ru>
2022-10-19 23:04:24 +0300

    typo

--- list comprehansion.py
+++ list comprehansion.py
@@ -2,19 +2,16 @@ def is_int(x):
     return isinstance(x, int)
 
 def each2d(test, matrix):
-    b = [elem_1 for elem_1 in [[test(elem) for elem in one_list] for one_list in matrix]]
-    #print(all(all(elem_1) for elem_1 in ((test(elem) for elem in one_list) for one_list in matrix)))
-    print(b)
+    return all(all(elem_1) for elem_1 in ((test(elem) for elem in one_list) for one_list in matrix))
 
 
 def some2d(test, matrix):
-    print(any(any(elem_1) for elem_1 in ((test(elem) for elem in one_list) for one_list in matrix)))
+    return any(any(elem_1) for elem_1 in ((test(elem) for elem in one_list) for one_list in matrix))
 
 
 def sum2d(test, matrix):
-    b = sum([sum([elem for elem in one_list if test(elem)]) for one_list in matrix])
-    #print(sum(elem for elem in (elem_1 for elem_1 in (one_list for one_list in matrix)))
-    print(b)
+    return sum((sum((elem for elem in one_list if test(elem))) for one_list in matrix))
+
 
 
 a = [[1, 'T'], [9, 10]]
