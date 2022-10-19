b77ae0d61d435235f09cb15af9f7108fb1fad909
Sizov <wwwwwwwww@list.ru>
2022-10-19 19:07:23 +0300

    Добавил решение преподавателя

--- number_of_unique_letters.py
+++ number_of_unique_letters.py
@@ -1,4 +1,10 @@
-#def number_of_unique_letters(str_word):
-    #char in enumerate(str_word)
-str_word = ' '
-print({len(char): pos for pos, char in enumerate(str_word.lower()) if char.isalpha()==True})
+# мое решение
+def number_of_unique_letters(str_word):  # подсчет количества уникальных букв в слове или фразе (без пробелов)
+    return len({char: pos for pos, char in enumerate(str_word.lower())
+    if char.isalpha()
+    })
+
+
+# решение преподавателя
+def number_of_unique_letters(text):
+    return len({char.lower() for char in text if char.isalpha()})
