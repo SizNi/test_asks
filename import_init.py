7158269e9200eed9e20af566f0e734e5c697b762
Sizov <wwwwwwwww@list.ru>
2022-10-20 14:15:10 +0300

    испытание с инит файлом

---
+++ import_init.py
@@ -0,0 +1,18 @@
+from generation_functions import *
+#b = [elem for elem in dir() if 'm' in elem]
+dir_all = dir()
+print(dir)
+
+
+b = [elem for elem in dir_all if 'm' in elem]
+c = [elem for elem in dir_all if 'e' in elem]
+
+for elem in c:
+    b. append(elem)
+al = []
+
+for elem in b:
+    al.append(f'{elem}')
+print(al)
+print(b)
+# END
